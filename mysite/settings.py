99071941a46284b6561cb4b75cccc6a6cbfdd8a5
“Anton <tony.j.kaminski@gmail.com>
2023-02-24 13:12:46 +0100

    postgres documentation

--- mysite/settings.py
+++ mysite/settings.py
@@ -80,10 +80,13 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Library',
+        'USER': 'postgres',
+        'PASSWORD': '123',
     }
 }
+
 
 
 # Password validation
